ea90efa28b1b39d6948d9eecdeac7f2b7b3fc2cf
Tarek Loubani <tarek@tarek.org>
2018-11-01 02:23:37 -0400

    Add Email::Sender class to properly substitute reply_key in CC field. This solves Issue #2

--- plugin.rb
+++ plugin.rb
@@ -11,6 +11,7 @@ PLUGIN_NAME ||= "replyto-individual".freeze
 after_initialize do
   Email::MessageBuilder.class_eval do
     attr_reader :template_args
+
     ALLOW_REPLY_BY_EMAIL_HEADER = 'X-Discourse-Allow-Reply-By-Email'.freeze
 
     def header_args
@@ -28,7 +29,8 @@ after_initialize do
 
       if allow_reply_by_email?
         result[ALLOW_REPLY_BY_EMAIL_HEADER] = true
-        if @opts[:private_reply] == true
+        result['Reply-To'] = reply_by_email_address
+        if private_reply?
           result['Reply-To'] = reply_by_email_address
         else
           p = Post.find_by_id @opts[:post_id]
@@ -40,6 +42,27 @@ after_initialize do
       end
 
       result.merge(Email::MessageBuilder.custom_headers(SiteSetting.email_custom_headers))
+    end
+  end
+
+  // Fix the Email::Sender method to also insert the reply_key into CC
+
+  Email::Sender.class_eval do
+    def set_reply_key(post_id, user_id)
+      return unless user_id &&
+        post_id &&
+        header_value(Email::MessageBuilder::ALLOW_REPLY_BY_EMAIL_HEADER).present?
+
+      # use safe variant here cause we tend to see concurrency issue
+      reply_key = PostReplyKey.find_or_create_by_safe!(
+        post_id: post_id,
+        user_id: user_id
+      ).reply_key
+
+      @message.header['Reply-To'] =
+        header_value('Reply-To').gsub!("%{reply_key}", reply_key)
+      @message.header['CC'] =
+        header_value('CC').gsub!("%{reply_key}", reply_key)
     end
   end
 end
